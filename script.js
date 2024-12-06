5fd4535c03fa8f604a275c8a52c79d97319e6438
Your NameJabanes <erezhabani2003@gmail.com>
2024-12-06 09:54:58 +0200

    fixed validation and win condition

--- script.js
+++ script.js
@@ -1,52 +1,72 @@
 
 let randomNumbers = [];
-
+Valid_Guess = false
 function GenerateLottery() {
-    if (!isNaN(Guess) && Guess != ""){
+    if (Valid_Guess == true) {
         for (let i = 0; i < 7; i++) {
 
             randomNumbers.push(Math.floor(Math.random() * 42) + 1);
             if (randomNumbers.includes(randomNumbers[i] == false)) {
                 randomNumbers.push(Math.floor(Math.random() * 42) + 1);
             }
-    
-    
-    
+
+
+
         }
         randomNumbers = document.getElementById("Lottery").textContent = `The winning numbers are: ${randomNumbers}`
-    
-        if (randomNumbers.includes(Guess)) {
+
+        if (randomNumbers.includes(Guess.value)) {
             document.getElementById("Result").textContent = ("YOU WON THE LOTTERY!")
             console.log("win!");
-    
+
         }
         else {
             document.getElementById("Result").textContent = ("You lost this time...")
             console.log("lose!");
-    
+
         }
-    
+
     }
-    else{
-        document.getElementById("Result").textContent = ("You didnt guess anything!")
+    else {
+        document.getElementById("Result").textContent = ("I can't reveal without a vaild guess!")
     }
-    }
-    
+}
+
 
 
 function submit() {
+
     Guess = document.getElementById("inputNumber").value;
 
-        if(isNaN(Guess) || Guess == ""){
-            document.getElementById("Guess").textContent =  `Invalid guess`
+    if (Valid_Guess == false) {
+
+        if (isNaN(Guess)) {
+            document.getElementById("Guess").textContent = `Invalid guess`
         }
 
-        else if (Guess <= 42) {
-            document.getElementById("Guess").textContent = `Your Guess is:  ${Guess} `
+        else if (Guess == "") {
+            document.getElementById("Guess").textContent = `You didn't Guess anything!  ${Guess} `
         }
-        else {
+
+
+        else if (Guess > 42){
             document.getElementById("Guess").textContent = `${Guess} is out of range!`
         }
 
+        else {
+
+            document.getElementById("Guess").textContent = `Your Guess is:  ${Guess} ` 
+            return Valid_Guess = true
+    
+        }
+    }
 
 }
+
+
+
+
+
+
+
+
